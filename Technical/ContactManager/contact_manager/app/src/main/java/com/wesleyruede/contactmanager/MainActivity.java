bd088bba8af444ed51daa740abb070a11655d4d6
wesley-ruede <wesley.k.r1993@gmail.com>
2020-02-11 17:33:34 -0500

    Triva finsihed and A SQL app started called ContactManager

--- Technical/ContactManager/contact_manager/app/src/main/java/com/wesleyruede/contactmanager/MainActivity.java
+++ Technical/ContactManager/contact_manager/app/src/main/java/com/wesleyruede/contactmanager/MainActivity.java
@@ -1,8 +1,14 @@
 package com.wesleyruede.contactmanager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.wesleyruede.contactmanager.data.DatabaseHandler;
+import com.wesleyruede.contactmanager.model.Contact;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DatabaseHandler db = new DatabaseHandler(MainActivity.this);
+
+
+        Contact jeremy = new Contact();
+        jeremy.setName("jeremy");
+        jeremy.setPhoneNumber("7887878");
+
+        Contact tim = new Contact();
+        tim.setName("Tim");
+        tim.setPhoneNumber("34354665");
+
+        //  db.addContact(jeremy);
+
+        List<Contact> contactList = db.getAllContacts();
+
+        for(Contact contact: contactList) {
+            Log.d("MainActvity", "onCreate: " + contact.getPhoneNumber());
+        }
     }
 }
