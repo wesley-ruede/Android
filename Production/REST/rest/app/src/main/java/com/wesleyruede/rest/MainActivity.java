cf47023966f7adaa07560ae1c5f2a74c0614219d
wesley-ruede <wesley.k.r1993@gmail.com>
2020-06-15 18:06:06 -0400

    ui design is complete need to pick fonts to finish ui, 90% complete

--- Production/REST/rest/app/src/main/java/com/wesleyruede/rest/MainActivity.java
+++ Production/REST/rest/app/src/main/java/com/wesleyruede/rest/MainActivity.java
@@ -1,22 +1,34 @@
 package com.wesleyruede.rest;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.jakewharton.threetenabp.AndroidThreeTen;
-import com.wesleyruede.rest.data.DatabaseHandler;
-import com.wesleyruede.rest.model.Groups;
+import com.wesleyruede.rest.ui.ScheduleActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button scheduleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidThreeTen.init(this);
         setContentView(R.layout.activity_main);
+        scheduleButton = findViewById(R.id.schedule_button);
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScheduleActivity.class);
+                startActivity(intent);
+            }
+        });
+        /*
         Groups groups = new Groups();
         DatabaseHandler db = new DatabaseHandler(MainActivity.this);
-
         Groups monday_group_one = new Groups();
         monday_group_one.setGroupStartTime("9:00 AM");
         monday_group_one.setGroupEndTime("10:00 AM");
@@ -25,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         //db.mondayAddGroup(new Groups(1,"Monday","Step//Sponsorship","12:00 PM","1:00 PM"));
         //db.mondayAddGroup(new Groups(2,"Monday","All Recovery-YPR","8:15 PM","9:15 PM"));
         db.mondayAddGroup(monday_group_one);
-        Log.d("MainActivity", "onCreate: " + monday_group_one.getGroupStartTime() + " , " +monday_group_one.getGroupEndTime()+ " , " +monday_group_one.getGroupName()+ " , " +monday_group_one.getGroupDay());
+        Log.d("MainActivity", "onCreate: " + monday_group_one.getGroupStartTime() + " , " +monday_group_one.getGroupEndTime()+ " , " +monday_group_one.getGroupName()+ " , " +monday_group_one.getGroupDay());*/
+
+
     }
 }
---
+++ Production/REST/rest/app/src/main/java/com/wesleyruede/rest/ui/ScheduleActivity.java
@@ -0,0 +1,12 @@
+package com.wesleyruede.rest.ui;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.wesleyruede.rest.R;
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
